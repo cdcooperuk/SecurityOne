f1a1d4583a7ce19d07efccc264a03eb90c0f43ac
Colin Cooper <cdcooperuk@googlemail.com>
2013-08-11 22:18:10 +0100

    include alerts in serialized form

--- arduino/SensorNode/RoomState.cpp
+++ arduino/SensorNode/RoomState.cpp
@@ -8,25 +8,27 @@
 #include "RoomState.h"
 #include <inttypes.h>
 
-RoomState::RoomState() : protocol_version(1), sensor_id(1), 
-			contact1_alert(false),
-			contact2_alert(false),
-			contact3_alert(false),
-			pir_alert(false)
-			{}
+RoomState::RoomState() :
+		protocol_version(1), sensor_id(1), contact1_alert(false), contact2_alert(
+				false), contact3_alert(false), pir_alert(false) {
+}
 RoomState::RoomState(char* serialized_state) :
-			contact1_alert(false),
-			contact2_alert(false),
-			contact3_alert(false),
-			pir_alert(false)
-{
+		contact1_alert(false), contact2_alert(false), contact3_alert(false), pir_alert(
+				false) {
 	int pvInt;
 	int sidInt;
-	printf("constructor %s.\n",serialized_state);
-	sscanf(serialized_state, "<%02d%02d---->",	&pvInt, &sidInt);
+	int c1, c2, c3, p;
+	printf("constructor %s.\n", serialized_state);
+	sscanf(serialized_state, "[%02d%02d%1d%1d%1d%1d]", &pvInt, &sidInt, &c1,
+			&c2, &c3, &p);
+
 	printf("scan completed\n");
-	protocol_version = (uint8_t)pvInt;
-	sensor_id = (uint8_t)sidInt;
+	protocol_version = (uint8_t) pvInt;
+	sensor_id = (uint8_t) sidInt;
+	contact1_alert=c1;
+	contact2_alert=c2;
+	contact3_alert=c3;
+	pir_alert=p;
 }
 
 RoomState::~RoomState() {
@@ -34,7 +36,8 @@ RoomState::~RoomState() {
 }
 
 char* RoomState::toString(char* buf) {
-	sprintf(buf,"<%02d%02d---->",protocol_version, sensor_id);
+	sprintf(buf, "[%02d%02d%1d%1d%1d%1d]", protocol_version, sensor_id,
+			contact1_alert, contact2_alert, contact3_alert, pir_alert);
 	return buf;
 
 }
